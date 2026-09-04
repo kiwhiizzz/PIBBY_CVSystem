69f5557bef365876236352d968068e21111b4e12
Ghiis <ghiis.soto.g@uni.pe>
2026-09-03 19:45:32 -0500

    Correcion de lo prestablecido

--- src/training/dataset_loader.py
+++ src/training/dataset_loader.py
@@ -1,6 +1,16 @@
 import os 
 import cv2
 
+IMG_SIZE = 224
+
+EMOTION_LABELS = {
+    "happy" : 0,
+    "sad" : 1,
+    "neutral" : 2,
+    "angry" :  3,
+    "fear" : 4,
+}
+
 def list_images_class(base_path):
     classes = os.listdir(base_path)
 
@@ -10,25 +20,22 @@ def list_images_class(base_path):
     for class_name in classes:
         class_folder = os.path.join(base_path, class_name)
         image_names = os.listdir(class_folder)
+
+        if class_name not in EMOTION_LABELS:
+            continue;
+
+        label  = EMOTION_LABELS[class_name]
+
         for image_name in image_names:
             image_path = os.path.join(class_folder, image_name)
             image = load_resize_image(image_path)
 
             images_data.append(image)
-            labels_data.append(class_name)
+            labels_data.append(label)
 
     return images_data, labels_data
-
-IMG_SIZE = 224
 
 def load_resize_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
     return image
-
-EMOTION_LABELS = {
-    "happy" = 0,
-    "sad" = 1,
-    "neutral" = 2,
-    "angry" =  3
-}
